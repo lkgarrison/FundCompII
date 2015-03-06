75c19e8b7168dbe4cf5b59aec315f970fdf897ca
Luke Garrison <lgarriso@nd.edu>
2015-03-06 02:44:10 -0500

    Streamlined sudoku solver driver file

--- lab6/main.cpp
+++ lab6/main.cpp
@@ -1,6 +1,5 @@
 /* Luke Garrison, main.cpp
- * This driver program will demo the Puzzle class with a 9x9 sudoku.
- * This class will also work with a 16x16 puzzle.
+ * This driver program will demo the Puzzle class solving a 9x9 sudoku puzzle
 */
 
 #include <iostream>
@@ -17,9 +16,4 @@ int main() {
 	sudoku.solve();
 	sudoku.display();
 
-
-
-	// uncomment the code below the test a 16x16 puzzle
-	// Puzzle<int> bigSudoku(16);	// 16x16 sudoku
-	// bigSudoku.play();
 }
